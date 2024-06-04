5a17123932a61212c8c01183b9c7e030e5808c3a
jihye1116 <jihyesim170@gmail.com>
2024-06-04 19:22:50 +0900

    delete: 기존에 있던 컴포넌트 삭제

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,7 @@
-import { Image, StyleSheet, Platform } from "react-native";
-
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle"></ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+  return <View></View>;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
+const styles = StyleSheet.create({});
